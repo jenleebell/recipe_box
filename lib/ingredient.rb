5a5e7b4574b7b3f4232bb7ca999cfa512bb0d8d9
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-26 12:15:48 -0700

    Add downcase callbacks to Ingredient class.

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,4 +1,18 @@
 class Ingredient < ActiveRecord::Base
-has_and_belongs_to_many(:recipes)
-validates(:name, :presence => true)
+  has_and_belongs_to_many(:recipes)
+  validates(:name, :presence => true)
+  before_save(:downcase_name)
+  before_save(:downcase_amount)
+
+private
+
+  define_method(:downcase_name) do
+    self.name=(name().downcase())
+  end
+
+  define_method(:downcase_amount) do
+    if self.amount() != nil
+      self.amount=(amount().downcase())
+    end
+  end
 end
--- spec/ingredient_spec.rb
+++ spec/ingredient_spec.rb
@@ -2,14 +2,24 @@ require('spec_helper')
 
 describe(Ingredient) do
   it('validates the presence of name') do
-    test_ingredient = Ingredient.new({:name => ""})
+    test_ingredient = Ingredient.new({:name => "", :amount => "1/2 TABLESPOON"})
     expect(test_ingredient.save()).to(eq(false))
+  end
+
+  it('converts the name to lowercase') do
+    test_ingredient = Ingredient.create({:name => "YOGURT", :amount => "1/2 TABLESPOON"})
+    expect(test_ingredient.name()).to(eq("yogurt"))
+  end
+
+  it('converts the amount to lowercase') do
+    test_ingredient = Ingredient.create({:name => "YOGURT", :amount => "1/2 TABLESPOON"})
+    expect(test_ingredient.amount()).to(eq("1/2 tablespoon"))
   end
 
   describe('#recipes') do
     it('lists all the recipes in it') do
       test_recipe = Recipe.create({:name => "yogurt and berries", :instructions => "mix yogurt and berries", :rating => 5})
-      test_ingredient = Ingredient.create({:name => "yogurt"})
+      test_ingredient = Ingredient.create({:name => "yogurt", :amount => "1/2 TABLESPOON"})
       test_ingredient.recipes.push(test_recipe)
       expect(test_ingredient.recipes()).to(eq([test_recipe]))
     end
