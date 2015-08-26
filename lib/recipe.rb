ebf48e5bab1b14c96c8184ff9b945b87bf80c742
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-26 11:16:53 -0700

    Add validations for Recipe class, pass.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ActiveRecord::Base
   has_and_belongs_to_many(:categories)
+  validates(:name, {:presence => true, :length => { :maximum => 50}})
+  validates(:instructions, :presence => true)
+
 end
