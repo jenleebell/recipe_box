cd2cffddeea007ee90ce71870011e12f46c1575f
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-26 11:53:26 -0700

    Add ingredients_recipe join table to database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826184746) do
+ActiveRecord::Schema.define(version: 20150826185219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 20150826184746) do
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.string   "amount"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "ingredients_recipes", force: :cascade do |t|
+    t.integer  "ingredient_id"
+    t.integer  "recipe_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
