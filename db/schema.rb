b51ffd92a94ad05fc2761e7ed0483795f66b1ec8
Alexa Zeazas & Jennifer Bell <student@epicodus.com>
2015-08-26 11:43:42 -0700

    Add description column to Recipes table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826173330) do
+ActiveRecord::Schema.define(version: 20150826184244) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150826173330) do
     t.integer  "rating"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
 end
